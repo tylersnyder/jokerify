81e22d65806747b1f1598b63d97938a3965744fb
Jonathan Basniak <jon@bluedreamers.com>
2017-07-06 12:24:08 -0400

    Update jokerify.js
    mack joker baby

--- jokerify.js
+++ jokerify.js
@@ -6,6 +6,8 @@ const { tmpdir } = require('os')
 const xss = require('xss')
 
 module.exports = jokerify
+module.exports = slacker
+module.exports = jokar
 
 async function jokerify(req, res) {
   try {
@@ -34,27 +36,42 @@ async function jokerify(req, res) {
     }
 
     joker.resize(image.bitmap.width * 0.5, jimp.AUTO)
+	
+    const joker_width = image.bitmap.width - joker.bitmap.width;
+	const joker_height = image.bitmap.height - joker.bitmap.height;
     
-    image
+	image
       .composite(
         joker,
-        image.bitmap.width - joker.bitmap.width,
-        image.bitmap.height - joker.bitmap.height
+        joker_width,
+        joker_height
       )
       .write(`${dir}/${filename}`)
 
-    res.send({
+    return {
       response_type: 'in_channel',
       'attachments': [
           {
-            'image_url': `${req.protocol}://${req.get('host')}/${filename}`
+            'image_url': `${req.protocol}://${req.get('host')}/${filename}`,
+			'width': joker_width,
+			'height': joker_height
           }
       ]
-    })
+    };
+	
   } catch(err) {
     const message = err.message || err
 
     res.status(500)
        .send(message)
   }
+}
+
+async function slacker(req, res) {
+	res.send(jokerify(req,res));
+}
+
+async function jokar(req, res) {
+	const joker = jokerify(req, res)).attachments[0];
+	res.send(`<img src="${joker.image_url}" width="${joker.width}" height="${joker.height}" />`);
 }
