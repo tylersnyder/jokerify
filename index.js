01f0b4adcd7c7ce6adb186a47debb21fe50b2923
= <tylersnyder@gmail.com>
2017-07-18 12:27:21 -0400

    cleanup

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ const express = require('express')
 const app = express()
 const jokerify = require('./jokerify')
 const { tmpdir } = require('os')
-const dir = tmpdir()
-const request = require('request')
+const root = tmpdir()
+const { post } = require('request')
 
-app.use(express.static(dir))
+app.use(express.static(root))
 
 app.get('/', async (req, res) => {
   try {
     const result = await jokerify(req, res)
     const { filename } = result.attachments[0]
-    res.sendFile(filename, {
-      root: dir
-    })
+    res.sendFile(filename, { root })
   } catch(err) {
+    console.log(err)
     res.status(500)
        .send(err)
   }
@@ -25,19 +24,24 @@ app.get('/api/slack', async (req, res) => {
     res.send({'text':'working on it...'})
 
     const result = await jokerify(req, res)
+    const { response_url } = result
+
+    if (response_url) {
+      const payload = {
+        response_type: 'in_channel',
+        response_url: result.responseUrl,
+        attachments: result.attachments
+      }
+
+      console.log('slack response url: ', response_url)
+      console.log('slack post payload: ', payload)
     
-    if(result.response_url.length > 0){
-      r.post(result.response_url,{json:
-        {
-          response_type: 'in_channel',
-          response_url: result.responseUrl,
-          attachments: result.attachments
-        }
-      });
+      post(response_url, { json: payload })
     }
-    
   } catch(err) {
-      console.log(err);
+    console.log(err);
+    res.status(500)
+       .send(err)
   }
 })
 
