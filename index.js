1acffaef4d007968298b6b3911a64760087709e5
Gary Cowgill <gcowgill@merrymtg.local>
2017-07-18 09:33:36 -0400

    Fix how request to response_url is generated for slack

--- index.js
+++ index.js
@@ -25,19 +25,15 @@ app.get('/api/slack', async (req, res) => {
     res.send({'text':'working on it...'})
 
     const result = await jokerify(req, res)
+    
     if(result.response_url.length > 0){
-      request.post(
-      result.response_url,
+      r.post(result.response_url,{json:
         {
           response_type: 'in_channel',
-          attachments: [
-              {
-                filename: result.filename,
-                image_url: result.image_url,
-              }
-          ]
+          response_url: result.responseUrl,
+          attachments: result.attachments
         }
-      );
+      });
     }
     
   } catch(err) {
