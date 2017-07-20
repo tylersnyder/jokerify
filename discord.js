9cdaf497b7d3a4e27c6fd5930839c8aa1872b867
Jon Basniak <jon@bluedreamers.com>
2017-07-20 12:39:15 -0400

    Remember to log bot into Discord, add debug mode setting, add method that we will use to grant permissions

--- discord.js
+++ discord.js
@@ -1,18 +1,21 @@
-const Discord = require('discord.js');
+const { Https } = require('https')
+const Discord = require('discord.js')
 const client = new Discord.Client()
 
 // SETTINGS
+const client_id = ''
 const token = ''
 const max_arguments = 3
-
+const debug_mode = false
 // END SETTINGS
+
 class message_handler {
     constructor(command_type, callback) {
         if (!command_type || (!callback || !callback instanceof Promise))
             throw new Error(`InvalidArgumentException: Malformed message_handler Registration.`)
 
         this.type = command_type
-        this.emit = (cmd, parsed_message) => callback(cmd, parsed_message)
+        this.emit = callback(cmd, parsed_message)
 
         this.emit.catch(error => this.handle_error)
     }
@@ -32,6 +35,10 @@ class discord_driver {
 
         client.on('ready', () => this.onReady)
         client.on('message', () => this.onMessage)
+        if (debug_mode) client.on('debug', (info) => console.info(info))
+
+        client.login(token)
+            .catch(error => console.error(error))
     }
 
     static client() { return client }
@@ -47,11 +54,19 @@ class discord_driver {
         console.log(`[Discord]: Registered '${handler.type}' as a message handler.`)
     }
 
+    async authenticate() {
+        return https.get(`https://discordapp.com/oauth2/authorize?&client_id=${client_id}&scope=bot&permissions=0`, (res) => {
+            //TODO: Figure out what the API call is to authorize bot to server.
+        })
+    }
+
     onReady() {
         console.log('Discord Driver online.')
     }
 
     onMessage(message) {
+        console.log('on message')
+        console.log(message)
         const parsed_message = parseMessageContent(message.content)
         const cmd = parsed_message[parsed_message.length - 1].startsWith('/')
             ? parsed_message.pop()
